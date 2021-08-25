ccccf3e1ced6d11e27b0883e64f209d5095996d0
LCDZhao-O <weizhao@oppo.com>
2021-08-25 17:18:44 +0800

    add limitRefreshPeriod to testAsyncTriggerRateLimit1

--- servicekeeper-core/src/test/java/esa/servicekeeper/core/executionchain/async/AsyncExecutionChainTest.java
+++ servicekeeper-core/src/test/java/esa/servicekeeper/core/executionchain/async/AsyncExecutionChainTest.java
@@ -41,6 +41,7 @@ import esa.servicekeeper.core.moats.ratelimit.RateLimitMoat;
 import esa.servicekeeper.core.utils.RandomUtils;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -165,7 +166,8 @@ class AsyncExecutionChainTest {
         final int limitForPeriod = RandomUtils.randomInt(5);
         List<Moat<?>> moats = Collections.singletonList(new RateLimitMoat(
                 new MoatConfig(ResourceId.from(name)),
-                RateLimitConfig.builder().limitForPeriod(limitForPeriod).build(), null,
+                RateLimitConfig.builder().limitForPeriod(limitForPeriod).limitRefreshPeriod(Duration.ofSeconds(10L)).build(),
+                null,
                 Collections.emptyList()));
         AsyncExecutionChain chain = new AsyncExecutionChainImpl(moats,
                 new FallbackToException(fallbackException, false));
